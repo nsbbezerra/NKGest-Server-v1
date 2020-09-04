8cf2ef8569d0694b85bbb76f8550a1684b1a8596
nsbbezerra <nsbbezerra@hotmail.com>
2020-09-04 18:49:40 -0300

    create references orders

--- src/app/models/ordenServico.js
+++ src/app/models/ordenServico.js
@@ -1,121 +1,125 @@
-const mongoose = require('../../database/database');
+const mongoose = require("../../database/database");
 const Schema = mongoose.Schema;
 
 const OrderServicoSchema = new Schema({
-
-    client: {
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: "Clientes",
+    required: true,
+  },
+  number: Number,
+  funcionario: {
+    type: Schema.Types.ObjectId,
+    ref: "Funcionarios",
+    required: true,
+  },
+  address: {
+    type: Schema.Types.ObjectId,
+    ref: "Enderecos",
+    required: true,
+  },
+  equipo: String,
+  marca: String,
+  modelo: String,
+  referencia: String,
+  services: [
+    {
+      quantity: Number,
+      service: {
         type: Schema.Types.ObjectId,
-        ref: 'Clientes',
-        required: true
-    },
-    number: Number,
-    funcionario: {
-        type: Schema.Types.ObjectId,
-        ref: 'Funcionarios',
-        required: true
-    },
-    address: {
-        type: Schema.Types.ObjectId,
-        ref: 'Enderecos',
-        required: true
-    },
-    veicles: {
-        type: Schema.Types.ObjectId,
-        ref: 'Veiculos',
-    },
-    services: [{
-        quantity: Number,
-        service: {
-            type: Schema.Types.ObjectId,
-            ref: 'Servicos'
-        },
-        name: {
-            type: String
-        },
-        valueUnit: {
-            type: Number
-        },
-        valueTotal: {
-            type: Number
-        }
-    }],
-    statuSales: {
+        ref: "Servicos",
+      },
+      name: {
         type: String,
-        enum: ['orca', 'sale', 'wait'],
-        required: true
-    },
-    statusPay: {
-        type: String,
-        enum: ['wait', 'pay'],
-        required: true,
-        default: 'wait'
-    },
-    desconto: {
-        type: Number
-    },
-    valueLiquido: {
+      },
+      valueUnit: {
         type: Number,
-    },
-    valueBruto: {
+      },
+      valueTotal: {
         type: Number,
+      },
     },
-    createDate: {
-        type: String,
-        required: true
+  ],
+  statuSales: {
+    type: String,
+    enum: ["orca", "sale", "wait"],
+    required: true,
+  },
+  statusPay: {
+    type: String,
+    enum: ["wait", "pay"],
+    required: true,
+    default: "wait",
+  },
+  desconto: {
+    type: Number,
+  },
+  valueLiquido: {
+    type: Number,
+  },
+  valueBruto: {
+    type: Number,
+  },
+  createDate: {
+    type: String,
+    required: true,
+  },
+  dateSave: {
+    type: Date,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  planoDeConta: {
+    type: Schema.Types.ObjectId,
+    ref: "PlanoContas",
+    required: true,
+  },
+  waiting: {
+    type: String,
+    enum: ["none", "yes", "cancel"],
+    required: true,
+  },
+  finish: {
+    type: String,
+    enum: ["no", "yes"],
+    required: true,
+    default: "no",
+  },
+  payments: [
+    {
+      paymentName: String,
+      paymentParcela: Number,
+      paymentValue: Number,
     },
-    dateSave: {
-        type: Date,
-        required: true
-    },
-    month: {
-        type: String,
-        required: true
-    },
-    year: {
-        type: Number,
-        required: true
-    },
-    planoDeConta: {
-        type: Schema.Types.ObjectId,
-        ref: 'PlanoContas',
-        required: true
-    },
-    waiting: {
-        type: String,
-        enum: ['none', 'yes', 'cancel'],
-        required: true
-    },
-    finish: {
-        type: String,
-        enum: ['no', 'yes'],
-        required: true,
-        default: 'no'
-    },
-    payments: [{
-        paymentName: String,
-        paymentParcela: Number,
-        paymentValue: Number
-    }],
-    nfse: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    nfseStatus: String,
-    danfeUrl: String,
-    xmlUrl: String,
-    caixa: {
-        type: String
-    },
-    boleto: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    obs: String,
-    rascunho: Boolean
+  ],
+  nfse: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  nfseStatus: String,
+  danfeUrl: String,
+  xmlUrl: String,
+  caixa: {
+    type: String,
+  },
+  boleto: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  obs: String,
+  rascunho: Boolean,
+  referenced: Boolean, //CAMPO NOVO
 });
 
-const ordersServico = mongoose.model('OrdensServico', OrderServicoSchema);
+const ordersServico = mongoose.model("OrdensServico", OrderServicoSchema);
 
 module.exports = ordersServico;
