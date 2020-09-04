3e368cb7979c9076d49090571c10913520be70e1
nsbbezerra <nsbbezerra@hotmail.com>
2020-09-04 15:22:02 -0300

    create template of print sales

---
+++ src/app/controllers/printer.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+const ejs = require("ejs");
+const pdf = require("html-pdf");
+const path = require("path");
+const fs = require("fs");
+const dateFns = require("date-fns");
+const Order = require("../models/ordenServico");
+const Sale = require("../models/ordens");
+const Address = require("../models/enderecos");
+const Clients = require("../models/clientes");
+const Company = require("../models/dadosEmpresa");
+const url = require("../../configs/logoUrl.json");
+
+//IMPRIMIR VENDA
+router.post("/sale", async (req, res) => {
+  const { id, mode } = req.body;
+  try {
+    const sale = await Sale.findOne({ _id: id });
+    const client = await Clients.findOne({ _id: sale.client });
+    const address = await Address.findOne({ client: sale.client });
+    const company = await Company.findOne();
+    const pathToFile = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "uploads",
+      "pdf"
+    );
+    const pathToTemplateSimplify = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "order-simplify.ejs"
+    );
+    const pathToTemplate = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "order.ejs"
+    );
+    var template =
+      mode === "matricial" ? pathToTemplateSimplify : pathToTemplate;
+    var link;
+    await ejs.renderFile(
+      template,
+      {
+        company: company,
+        client: client,
+        address: address,
+        sale: sale,
+        logo: `${url.logoUrl}/img/${company.logo}`,
+        products: sale.products,
+        payments: sale.payments.length ? sale.payments : [],
+      },
+      (err, html) => {
+        if (err) {
+          console.log("EJS", err);
+          return res
+            .status(400)
+            .send({ message: "Erro ao gerar o relatório para impressão" });
+        } else {
+          pdf
+            .create(html, {
+              format: mode === "matricial" ? "A5" : "A4",
+              orientation: mode === "matricial" ? "landscape" : "portrait",
+              border: {
+                bottom: "1cm",
+                top: "1cm",
+                right: "1cm",
+                left: "1cm",
+              },
+              httpHeaders: {
+                "Content-Type": "application/pdf",
+              },
+            })
+            .toFile(
+              `${pathToFile}/venda-${sale.number}.pdf`,
+              (err, response) => {
+                if (err) {
+                  console.log("PDF", err);
+                  return res.status(400).send({
+                    message: "Erro ao gerar o relatório para impressão",
+                  });
+                } else {
+                  console.log("OK");
+                }
+              }
+            );
+        }
+      }
+    );
+    link = `${url.logoUrl}/pdf/venda-${sale.number}.pdf`;
+    return res.status(200).send({ link });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ message: "Erro ao gerar o relatório para impressão" });
+  }
+});
+
+module.exports = (app) => app.use("/printer", router);
--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const path = require('path');
-const initConfig = require('./app/controllers/initController');
+const path = require("path");
+const initConfig = require("./app/controllers/initController");
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use('/xml',express.static(path.resolve(__dirname, 'src', 'public', 'xml')));
-app.use('/img',express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  "/xml",
+  express.static(path.resolve(__dirname, "src", "public", "xml"))
+);
+app.use("/img", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(
+  "/pdf",
+  express.static(path.resolve(__dirname, "..", "uploads", "pdf"))
+);
 
-require('./app/controllers/administrativo')(app);
-require('./app/controllers/cadastros')(app);
-require('./app/controllers/caixa')(app);
-require('./app/controllers/estoque')(app);
-require('./app/controllers/financeiro')(app);
-require('./app/controllers/relatorios')(app);
-require('./app/controllers/vendas')(app);
-require('./app/controllers/configuracoes')(app);
-require('./app/controllers/pagamentos')(app);
-require('./app/controllers/comissoes')(app);
-require('./app/controllers/balancete')(app);
-require('./app/controllers/fornecedores')(app);
-require('./app/controllers/nfe')(app);
-require('./app/controllers/nfce')(app);
-require('./app/controllers/xmlImporter')(app);
+require("./app/controllers/administrativo")(app);
+require("./app/controllers/cadastros")(app);
+require("./app/controllers/caixa")(app);
+require("./app/controllers/estoque")(app);
+require("./app/controllers/financeiro")(app);
+require("./app/controllers/relatorios")(app);
+require("./app/controllers/vendas")(app);
+require("./app/controllers/configuracoes")(app);
+require("./app/controllers/pagamentos")(app);
+require("./app/controllers/comissoes")(app);
+require("./app/controllers/balancete")(app);
+require("./app/controllers/fornecedores")(app);
+require("./app/controllers/nfe")(app);
+require("./app/controllers/nfce")(app);
+require("./app/controllers/xmlImporter")(app);
+require("./app/controllers/printer")(app);
 
 const PORT = 5000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
-app.listen(PORT, HOST,function() {
-    console.log('Servidor ativo na porta %s', PORT);
-    initConfig.init();
-})
+app.listen(PORT, HOST, function () {
+  console.log("Servidor ativo na porta %s", PORT);
+  initConfig.init();
+});
