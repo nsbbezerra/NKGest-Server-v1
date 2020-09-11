608089475903a1eb6e92d73da54339a0d3dd8417
nsbbezerra <nsbbezerra@hotmail.com>
2020-09-11 16:26:08 -0300

    create other controllers of print

--- src/app/controllers/printRelatorio.js
+++ src/app/controllers/printRelatorio.js
@@ -85,4 +85,214 @@ router.post("/clients", async (req, res) => {
   }
 });
 
+router.post("/products", async (req, res) => {
+  try {
+    const clientes = await Products.find();
+    const company = await Company.findOne();
+    const pathToFile = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "uploads",
+      "relatorio"
+    );
+    const pathToTemplate = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "produtos.ejs"
+    );
+    await ejs.renderFile(
+      pathToTemplate,
+      {
+        company: company,
+        products: clientes,
+        logo: `${configs.logoUrl}/img/${company.logo}`,
+      },
+      (err, html) => {
+        if (err) {
+          console.log("EJS", err);
+          return res
+            .status(400)
+            .send({ message: "Erro ao gerar o relatório para impressão" });
+        } else {
+          pdf
+            .create(html, {
+              format: "A4",
+              orientation: "portrait",
+              border: {
+                bottom: "1cm",
+                top: "1cm",
+                right: "1cm",
+                left: "1cm",
+              },
+              httpHeaders: {
+                "Content-Type": "application/pdf",
+              },
+            })
+            .toFile(`${pathToFile}/produtos.pdf`, (err, response) => {
+              if (err) {
+                console.log("PDF", err);
+                return res.status(400).send({
+                  message: "Erro ao gerar o relatório para impressão",
+                });
+              } else {
+                console.log("OK");
+              }
+            });
+        }
+      }
+    );
+    const link = `${configs.logoUrl}/relatorio/produtos.pdf`;
+    return res.status(200).send({ link });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ message: "Erro ao gerar o relatório para impressão" });
+  }
+});
+
+router.post("/services", async (req, res) => {
+  try {
+    const clientes = await Services.find();
+    const company = await Company.findOne();
+    const pathToFile = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "uploads",
+      "relatorio"
+    );
+    const pathToTemplate = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "servicos.ejs"
+    );
+    await ejs.renderFile(
+      pathToTemplate,
+      {
+        company: company,
+        services: clientes,
+        logo: `${configs.logoUrl}/img/${company.logo}`,
+      },
+      (err, html) => {
+        if (err) {
+          console.log("EJS", err);
+          return res
+            .status(400)
+            .send({ message: "Erro ao gerar o relatório para impressão" });
+        } else {
+          pdf
+            .create(html, {
+              format: "A4",
+              orientation: "portrait",
+              border: {
+                bottom: "1cm",
+                top: "1cm",
+                right: "1cm",
+                left: "1cm",
+              },
+              httpHeaders: {
+                "Content-Type": "application/pdf",
+              },
+            })
+            .toFile(`${pathToFile}/servicos.pdf`, (err, response) => {
+              if (err) {
+                console.log("PDF", err);
+                return res.status(400).send({
+                  message: "Erro ao gerar o relatório para impressão",
+                });
+              } else {
+                console.log("OK");
+              }
+            });
+        }
+      }
+    );
+    const link = `${configs.logoUrl}/relatorio/servicos.pdf`;
+    return res.status(200).send({ link });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ message: "Erro ao gerar o relatório para impressão" });
+  }
+});
+
+router.post("/fornecers", async (req, res) => {
+  try {
+    const clientes = await Fornecers.find();
+    const company = await Company.findOne();
+    const pathToFile = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "uploads",
+      "relatorio"
+    );
+    const pathToTemplate = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "fornecedores.ejs"
+    );
+    await ejs.renderFile(
+      pathToTemplate,
+      {
+        company: company,
+        fornecers: clientes,
+        logo: `${configs.logoUrl}/img/${company.logo}`,
+      },
+      (err, html) => {
+        if (err) {
+          console.log("EJS", err);
+          return res
+            .status(400)
+            .send({ message: "Erro ao gerar o relatório para impressão" });
+        } else {
+          pdf
+            .create(html, {
+              format: "A4",
+              orientation: "portrait",
+              border: {
+                bottom: "1cm",
+                top: "1cm",
+                right: "1cm",
+                left: "1cm",
+              },
+              httpHeaders: {
+                "Content-Type": "application/pdf",
+              },
+            })
+            .toFile(`${pathToFile}/fornecedores.pdf`, (err, response) => {
+              if (err) {
+                console.log("PDF", err);
+                return res.status(400).send({
+                  message: "Erro ao gerar o relatório para impressão",
+                });
+              } else {
+                console.log("OK");
+              }
+            });
+        }
+      }
+    );
+    const link = `${configs.logoUrl}/relatorio/fornecedores.pdf`;
+    return res.status(200).send({ link });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ message: "Erro ao gerar o relatório para impressão" });
+  }
+});
+
 module.exports = (app) => app.use("/printerRelatorio", router);
