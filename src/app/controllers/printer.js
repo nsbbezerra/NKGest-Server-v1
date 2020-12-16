c22af9848142b26fb67dd8552a941605e66cc725
Natanael Bezerra <nsbbezerra@hotmail.com>
2020-12-16 14:46:12 -0300

    add thermal printer template

--- src/app/controllers/printer.js
+++ src/app/controllers/printer.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const ejs = require("ejs");
 const pdf = require("html-pdf");
 const path = require("path");
-const fs = require("fs");
-const dateFns = require("date-fns");
 const Order = require("../models/ordenServico");
 const Sale = require("../models/ordens");
 const Address = require("../models/enderecos");
 const Clients = require("../models/clientes");
 const Company = require("../models/dadosEmpresa");
 const url = require("../../configs/logoUrl.json");
+const HTMLParser = require("node-html-parser");
 
 //IMPRIMIR VENDA
 router.post("/sale", async (req, res) => {
@@ -35,6 +34,13 @@ router.post("/sale", async (req, res) => {
       "templates",
       "order-simplify.ejs"
     );
+    const pathToTemplateThermal = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "thermalOrder.ejs"
+    );
     const pathToTemplate = path.resolve(
       __dirname,
       "..",
@@ -42,8 +48,16 @@ router.post("/sale", async (req, res) => {
       "templates",
       "order.ejs"
     );
-    var template =
-      mode === "matricial" ? pathToTemplateSimplify : pathToTemplate;
+    var template;
+    if (mode === "matricial") {
+      template = pathToTemplateSimplify;
+    }
+    if (mode === "normal") {
+      template = pathToTemplate;
+    }
+    if (mode === "thermal") {
+      template = pathToTemplateThermal;
+    }
     var link;
     await ejs.renderFile(
       template,
@@ -58,38 +72,75 @@ router.post("/sale", async (req, res) => {
       },
       (err, html) => {
         if (err) {
-          console.log("EJS", err);
           return res
             .status(400)
             .send({ message: "Erro ao gerar o relatório para impressão" });
         } else {
-          pdf
-            .create(html, {
-              format: mode === "matricial" ? "A5" : "A4",
-              orientation: mode === "matricial" ? "landscape" : "portrait",
-              border: {
-                bottom: "1cm",
-                top: "1cm",
-                right: "1cm",
-                left: "1cm",
-              },
-              httpHeaders: {
-                "Content-Type": "application/pdf",
-              },
-            })
-            .toFile(
-              `${pathToFile}/venda-${sale.number}.pdf`,
-              (err, response) => {
-                if (err) {
-                  console.log("PDF", err);
-                  return res.status(400).send({
-                    message: "Erro ao gerar o relatório para impressão",
-                  });
-                } else {
-                  console.log("OK");
+          if (mode === "matricial" || mode === "normal") {
+            pdf
+              .create(html, {
+                format: mode === "matricial" ? "A5" : "A4",
+                orientation: mode === "matricial" ? "landscape" : "portrait",
+                border: {
+                  bottom: "1cm",
+                  top: "1cm",
+                  right: "1cm",
+                  left: "1cm",
+                },
+                httpHeaders: {
+                  "Content-Type": "application/pdf",
+                },
+              })
+              .toFile(
+                `${pathToFile}/venda-${sale.number}.pdf`,
+                (err, response) => {
+                  if (err) {
+                    console.log("PDF", err);
+                    return res.status(400).send({
+                      message: "Erro ao gerar o relatório para impressão",
+                    });
+                  } else {
+                    console.log("OK");
+                  }
                 }
+              );
+          } else {
+            var initialHeight = 11;
+            const length = sale.products.length;
+            if (length > 1) {
+              for (let index = 0; index < length; index++) {
+                initialHeight = initialHeight + 0.5;
               }
-            );
+            }
+            const height = `${initialHeight.toString()}cm`;
+            pdf
+              .create(html, {
+                width: "8cm",
+                height: height,
+                border: {
+                  bottom: ".5cm",
+                  top: ".5cm",
+                  right: ".5cm",
+                  left: ".5cm",
+                },
+                httpHeaders: {
+                  "Content-Type": "application/pdf",
+                },
+              })
+              .toFile(
+                `${pathToFile}/venda-${sale.number}.pdf`,
+                (err, response) => {
+                  if (err) {
+                    console.log("PDF", err);
+                    return res.status(400).send({
+                      message: "Erro ao gerar o relatório para impressão",
+                    });
+                  } else {
+                    console.log("OK");
+                  }
+                }
+              );
+          }
         }
       }
     );
@@ -131,8 +182,23 @@ router.post("/order", async (req, res) => {
       "templates",
       "servico.ejs"
     );
-    var template =
-      mode === "matricial" ? pathToTemplateSimplify : pathToTemplate;
+    const pathToTemplateThermal = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "templates",
+      "thermalServ.ejs"
+    );
+    var template;
+    if (mode === "matricial") {
+      template = pathToTemplateSimplify;
+    }
+    if (mode === "normal") {
+      template = pathToTemplate;
+    }
+    if (mode === "thermal") {
+      template = pathToTemplateThermal;
+    }
     var link;
     await ejs.renderFile(
       template,
@@ -152,33 +218,72 @@ router.post("/order", async (req, res) => {
             .status(400)
             .send({ message: "Erro ao gerar o relatório para impressão" });
         } else {
-          pdf
-            .create(html, {
-              format: mode === "matricial" ? "A5" : "A4",
-              orientation: mode === "matricial" ? "landscape" : "portrait",
-              border: {
-                bottom: "1cm",
-                top: "1cm",
-                right: "1cm",
-                left: "1cm",
-              },
-              httpHeaders: {
-                "Content-Type": "application/pdf",
-              },
-            })
-            .toFile(
-              `${pathToFile}/ordem-${sale.number}.pdf`,
-              (err, response) => {
-                if (err) {
-                  console.log("PDF", err);
-                  return res.status(400).send({
-                    message: "Erro ao gerar o relatório para impressão",
-                  });
-                } else {
-                  console.log("OK");
+          if (mode === "matricial" || mode === "normal") {
+            console.log("ENTROU NO MATRICIAL");
+            pdf
+              .create(html, {
+                format: mode === "matricial" ? "A5" : "A4",
+                orientation: mode === "matricial" ? "landscape" : "portrait",
+                border: {
+                  bottom: "1cm",
+                  top: "1cm",
+                  right: "1cm",
+                  left: "1cm",
+                },
+                httpHeaders: {
+                  "Content-Type": "application/pdf",
+                },
+              })
+              .toFile(
+                `${pathToFile}/ordem-${sale.number}.pdf`,
+                (err, response) => {
+                  if (err) {
+                    console.log("PDF", err);
+                    return res.status(400).send({
+                      message: "Erro ao gerar o relatório para impressão",
+                    });
+                  } else {
+                    console.log("OK");
+                  }
                 }
+              );
+          } else {
+            var initialHeight = 11;
+            const length = sale.products.length;
+            if (length > 1) {
+              for (let index = 0; index < length; index++) {
+                initialHeight = initialHeight + 0.5;
               }
-            );
+            }
+            const height = `${initialHeight.toString()}cm`;
+            pdf
+              .create(html, {
+                width: "8cm",
+                height: height,
+                border: {
+                  bottom: ".5cm",
+                  top: ".5cm",
+                  right: ".5cm",
+                  left: ".5cm",
+                },
+                httpHeaders: {
+                  "Content-Type": "application/pdf",
+                },
+              })
+              .toFile(
+                `${pathToFile}/ordem-${sale.number}.pdf`,
+                (err, response) => {
+                  if (err) {
+                    console.log("PDF", err);
+                    return res.status(400).send({
+                      message: "Erro ao gerar o relatório para impressão",
+                    });
+                  } else {
+                    console.log("OK");
+                  }
+                }
+              );
+          }
         }
       }
     );
